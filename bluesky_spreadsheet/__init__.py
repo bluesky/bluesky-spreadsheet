d94a8287726585e33470f5eccb33dd371ee316f0
Dan Allan <dallan@bnl.gov>
2019-07-16 12:54:58 -0400

    Add sketch from conversation with @DanOlds.

--- bluesky_spreadsheet/__init__.py
+++ bluesky_spreadsheet/__init__.py
@@ -1,4 +1,24 @@
+import pandas
 
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+
+class ExcelSpreadsheet:
+    def __init__(self, filepath, plan, detectors, quiet=False):
+        self.filepath = filepath
+        self._current_row = 0
+        self.detectors = detectors
+        self.quiet = quiet
+
+    def __call__(self, start_at=None):
+        if start_at is not None:
+            self._current_row = start_at
+        while True:
+            df = pandas.from_excel(filepath)
+            row = df.iloc[self.current_row]
+            if not self.quiet:
+                print(row)
+            yield from self.plan(self.detectors, row)
+            self._curent_row += 1
--- bluesky_spreadsheet/tests/test_examples.py
+++ bluesky_spreadsheet/tests/test_examples.py
@@ -1,3 +1,16 @@
-def test_one_plus_one_is_two():
-    "Check that one and one are indeed two."
-    assert 1 + 1 == 2
+from pathlib import Path
+
+import pandas
+
+from .. import ExcelSpreadsheet
+
+
+def test_instantiation(tmp_path):
+    "Very basic test that does not actually *run* anything"
+
+    def plan(detectors, row):
+        ...
+
+    filepath = Path(tmp_path, 'test.xlsx')
+    pandas.DataFrame({'a': [1, 2]}).to_excel(filepath)
+    ExcelSpreadsheet(filepath, plan, [])
